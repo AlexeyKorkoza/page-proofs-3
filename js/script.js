014076f8eef3b6cbd619bc0be314cb1667ba059c
AlexeyKorkoza <alexeykorkoza@gmail.com>
2017-08-02 00:04:35 +0300

    Add slider for clients

--- js/script.js
+++ js/script.js
@@ -23,3 +23,30 @@ $(".main-slider .owl-carousel").owlCarousel({
 
 $(".main-slider .owl-prev").empty();
 $(".main-slider .owl-next").empty();
+
+$(".testimonials .owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  responsiveClass: true,
+  mouseDrag: false,
+  touchDrag: false,
+  pullDrag: false,
+  autoplay: true,
+  smartSpeed: 1000,
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 1
+    },
+    900: {
+      items: 1
+    }
+  }
+});
+
+$(".testimonials .owl-prev").empty();
+$(".testimonials .owl-next").empty();
+$(".testimonials .owl-dots").remove();
